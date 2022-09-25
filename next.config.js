2842f8bc05fc74b2f7d0a2e857333418a29dc2ef
= <31937542+iAverages@users.noreply.github.com>
2022-09-25 23:08:03 +0100

    Remove commented code
    This was me trying to get y-monaco to work with nextjs, for now i am not using it but in the future i will attempt to get it working.

--- next.config.js
+++ next.config.js
@@ -1,40 +1,7 @@
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin")
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-}
+};
 
-module.exports = nextConfig
-// const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-// const withTM = require("next-transpile-modules)(["monaco-editor"]);
-
-// module.exports = withTM({
-//     webpack: (config) => {
-//         const rule = config.module.rules
-//             .find((rule) => rule.oneOf)
-//             .oneOf.find(
-//                 (r) =>
-//                     // Find the global CSS loader
-//                     r.issuer && r.issuer.include && r.issuer.include.includes("_app")
-//             );
-//         if (rule) {
-//             rule.issuer.include = [
-//                 rule.issuer.include,
-//                 // Allow `monaco-editor` to import global CSS:
-//                 /[\\/]node_modules[\\/]monaco-editor[\\/]/,
-//             ];
-//         }
-
-//         config.plugins.push(
-//             new MonacoWebpackPlugin({
-//                 languages: ["typescript", "javascript"],
-//                 filename: "static/[name].worker.js",
-//             })
-//         );
-//         config.resolve.alias["monaco-editor"] = "/home/dan/notes/node_modules/monaco-editor";
-//         console.log(config.module.rules);
-//         return config;
-//     },
-// });
+module.exports = nextConfig;
