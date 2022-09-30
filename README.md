f696d7fdda566afe9bd4b26585ab6692646932b0
= <31937542+iAverages@users.noreply.github.com>
2022-09-30 18:38:09 +0100

    Update NGINX config in README

--- README.md
+++ README.md
@@ -32,26 +32,43 @@ yarn start
 
 ### SSL
 
-For SSL support, you will need a webserver to reverse proxy the application. Any webserver can be used, a basic Nginx configuration is below:
+For SSL support, you will need a webserver to reverse proxy the application. Any webserver can be used, a basic Nginx
+configuration is below:
 
 ```nginx
-// grab this once home
+upstream notes {
+        server localhost:3000;
+}
+
+server {
+        listen 443 ssl http2;
+        listen [::]:443 ssl http2;
+        server_name notes.example.com;
+
+        ssl_certificate /etc/nginx/ssl/cert.pem;
+        ssl_certificate_key /etc/nginx/ssl/cert.key;
+        ssl_trusted_certificate /etc/nginx/ssl/origin_ca_rsa_root.pem;
+
+        location / {
+                proxy_set_header Host $http_host;
+                proxy_pass http://notes;
+        }
+}
 ```
 
 ## Features
 
-- [x] Auto saving  
-- [x] Auto syncing between clients (Current impl is bad, but this does work) 
-- [x] Multiple notes 
-- [x] Dark & light mode
-- [x] Update notes via API
-- [x] Private notes
-- [ ] Public notes
-- [ ] Multi user (with profiles)
-- [ ] Note Templates
-- [ ] Archived notes
-- [ ] Revision system
-- [ ] Search notes
-- [ ] Language selector
-- [ ] Note categories
-
+-   [x] Auto saving
+-   [x] Auto syncing between clients (Current impl is bad, but this does work)
+-   [x] Multiple notes
+-   [x] Dark & light mode
+-   [x] Update notes via API
+-   [x] Private notes
+-   [ ] Public notes
+-   [ ] Multi user (with profiles)
+-   [ ] Note Templates
+-   [ ] Archived notes
+-   [ ] Revision system
+-   [ ] Search notes
+-   [ ] Language selector
+-   [ ] Note categories
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -36,63 +36,61 @@ const Nav: FC<IndexProps> = ({ title }) => {
     const { connected } = useWebsocket();
 
     return (
-        <>
-            <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-                <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-                    <Link href={"/"}>
-                        <Button>{<ArrowBackIcon />}</Button>
-                    </Link>
-                    <Box>{title}</Box>
+        <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+            <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+                <Link href={"/"}>
+                    <Button>{<ArrowBackIcon />}</Button>
+                </Link>
+                <Box>{title}</Box>
 
-                    <Flex alignItems={"center"}>
-                        <Stack direction={"row"} spacing={7}>
-                            <Popover>
-                                <PopoverTrigger>
-                                    <Button
-                                        bg={connected ? "green.500" : "red.600"}
-                                        _hover={{ bg: connected ? "green.700" : "red.700" }}
-                                    >
-                                        {connected ? <CheckIcon /> : <CloseIcon />}
-                                    </Button>
-                                </PopoverTrigger>
-                                <PopoverContent>
-                                    <PopoverArrow />
-                                    <PopoverCloseButton />
-                                    <PopoverHeader>{connected ? "Connected" : "Disconnected"}</PopoverHeader>
-                                    <PopoverBody>
-                                        {connected
-                                            ? "Websocket connection is active, notes will work as excepted"
-                                            : "Websocket disconnected. Notes will not update until connection is active again!"}
-                                    </PopoverBody>
-                                </PopoverContent>
-                            </Popover>
-                            <Button onClick={toggleColorMode}>{colorMode === "light" ? <MoonIcon /> : <SunIcon />}</Button>
+                <Flex alignItems={"center"}>
+                    <Stack direction={"row"} spacing={7}>
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button
+                                    bg={connected ? "green.500" : "red.600"}
+                                    _hover={{ bg: connected ? "green.700" : "red.700" }}
+                                >
+                                    {connected ? <CheckIcon /> : <CloseIcon />}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                <PopoverArrow />
+                                <PopoverCloseButton />
+                                <PopoverHeader>{connected ? "Connected" : "Disconnected"}</PopoverHeader>
+                                <PopoverBody>
+                                    {connected
+                                        ? "Websocket connection is active, notes will work as excepted"
+                                        : "Websocket disconnected. Notes will not update until connection is active again!"}
+                                </PopoverBody>
+                            </PopoverContent>
+                        </Popover>
+                        <Button onClick={toggleColorMode}>{colorMode === "light" ? <MoonIcon /> : <SunIcon />}</Button>
 
-                            <Menu>
-                                <MenuButton as={Button} rounded={"full"} variant={"link"} cursor={"pointer"} minW={0}>
-                                    <Avatar size={"sm"} src={"https://cdn.avrg.dev/screenshots/pfp.jpg"} />
-                                </MenuButton>
-                                <MenuList alignItems={"center"}>
-                                    <br />
-                                    <Center>
-                                        <Avatar size={"2xl"} src={"https://cdn.avrg.dev/screenshots/pfp.jpg"} />
-                                    </Center>
-                                    <br />
-                                    <Center>
-                                        <p>dan</p>
-                                    </Center>
-                                    <br />
-                                    <MenuDivider />
-                                    <Text marginLeft={2}>Commit: {process.env.COMMIT_HASH}</Text>
-                                    <MenuDivider />
-                                    <MenuItem>Logout</MenuItem>
-                                </MenuList>
-                            </Menu>
-                        </Stack>
-                    </Flex>
+                        <Menu>
+                            <MenuButton as={Button} rounded={"full"} variant={"link"} cursor={"pointer"} minW={0}>
+                                <Avatar size={"sm"} src={"https://cdn.avrg.dev/screenshots/pfp.jpg"} />
+                            </MenuButton>
+                            <MenuList alignItems={"center"}>
+                                <br />
+                                <Center>
+                                    <Avatar size={"2xl"} src={"https://cdn.avrg.dev/screenshots/pfp.jpg"} />
+                                </Center>
+                                <br />
+                                <Center>
+                                    <p>dan</p>
+                                </Center>
+                                <br />
+                                <MenuDivider />
+                                <Text marginLeft={2}>Commit: {process.env.COMMIT_HASH}</Text>
+                                <MenuDivider />
+                                <MenuItem>Logout</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Stack>
                 </Flex>
-            </Box>
-        </>
+            </Flex>
+        </Box>
     );
 };
 
